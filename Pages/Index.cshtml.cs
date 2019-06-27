099fdce9a558866bbe21096b5c734a6b601b557c
risa2 <blazek.risa@gmail.com>
2019-06-27 15:41:57 +0200

    Configuration in jobs.config

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -12,39 +12,15 @@ namespace DotnetBunnyLogBrowser.Pages
 {
 	public class IndexModel : PageModel
 	{
-		public string GetExampleLog() => "createdump-aspnet log:\n" + //TODO: delete example log
-			       "\n" +
-			       "+ version=2.1.504\n" +
-			       "+ dotnet new web --force\n" +
-			       "The template \"ASP.NET Core Empty\" was created successfully.\n" +
-			       "\n" +
-			       "Processing post-creation actions...\n" +
-			       "Running 'dotnet restore' on /mnt/ramdisk/dotnet-bunny/dotnet-regular-tests/createdump-aspnet/createdump-aspnet.csproj...\n" +
-			       "Restoring packages for /mnt/ramdisk/dotnet-bunny/dotnet-regular-tests/createdump-aspnet/createdump-aspnet.csproj...\n" +
-			       "Generating MSBuild file /mnt/ramdisk/dotnet-bunny/dotnet-regular-tests/createdump-aspnet/obj/createdump-aspnet.csproj.nuget.g.props.\n" +
-			       "    Generating MSBuild file /mnt/ramdisk/dotnet-bunny/dotnet-regular-tests/createdump-aspnet/obj/createdump-aspnet.csproj.nuget.g.targets.\n" +
-			       "    Restore completed in 1.22 sec for /mnt/ramdisk/dotnet-bunny/dotnet-regular-tests/createdump-aspnet/createdump-aspnet.csproj.\n" +
-			       "\n" +
-			       "Restore succeeded.\n" +
-			       "\n" +
-			       "+ dotnet tool install -g dotnet-dev-certs\n" +
-			       "Tool 'dotnet-dev-certs' is already installed.\n" +
-			       "+ true\n" +
-			       "+ dotnet dev-certs https\n" +
-			       "Cannot find command 'dotnet dev-certs', please run the following command to install\n" +
-			       "\n" +
-			       "dotnet tool install --global dotnet-dev-certs";
-
 		public List<BunnyJob> GetJobs()
 		{
-			string jobs_directory="/home/rblazek/Documents/dotnet-bunny-log-browser/TestData/" /*"http://hydra.brq.redhat.com:8080/view/"*/;
-			var job_names=Directory.GetDirectories(jobs_directory, "dotnet-rpm*");
+            var job_names=Directory.GetDirectories(JobsConfig.Get().JobsDirectory, JobsConfig.Get().JobsPrefix+"*");
 			var jobs=new List<BunnyJob>(job_names.Length);
 
 			for(int i=0; i<job_names.Length; ++i)
 			{
 				job_names[i]=job_names[i].Substring(job_names[i].LastIndexOf('/')+1);
-				var logfile=System.IO.File.ReadAllText(jobs_directory+job_names[i]+"/ws/results/logfile.log", Encoding.UTF8);
+				var logfile=System.IO.File.ReadAllText(JobsConfig.Get().JobsDirectory+job_names[i]+"/ws/results/logfile.log", Encoding.UTF8);
 				var errors=new List<int>(){0};
 				while(errors.Last()!=-1)
 				{
@@ -61,17 +37,11 @@ namespace DotnetBunnyLogBrowser.Pages
 					logfile=logfile.Substring(0, errors[j+1]);
 					var last_lf=logfile.LastIndexOf("\n");
 					var test_name=logfile.Substring(last_lf, logfile.IndexOf(":", last_lf)-last_lf).Trim();
-					tests.Add(new BunnyTest(test_name, System.IO.File.ReadAllText(jobs_directory+job_names[i]+"/ws/results/logfile-"+test_name+".log", Encoding.UTF8)));
+					tests.Add(new BunnyTest(test_name, System.IO.File.ReadAllText(JobsConfig.Get().JobsDirectory+job_names[i]+"/ws/results/logfile-"+test_name+".log", Encoding.UTF8)));
 				}
 				jobs.Add(new BunnyJob(job_names[i], false, tests));
 			}
 			return jobs;
-//			return new List<BunnyJob>(){
-//				new BunnyJob("dotnet-rpm-rhel7-x64-1.0", true),
-//				new BunnyJob("dotnet-rpm-rhel7-x64-1.1", true),
-//				new BunnyJob("dotnet-rpm-rhel7-x64-2.1", false, new List<BunnyTest>(){new BunnyTest("createdump-aspnet", GetExampleLog()), new BunnyTest("bash-completion", "Bash completion is bork and this is the example log.")}),
-//				new BunnyJob("dotnet-rpm-rhel7-x64-2.2", true)
-//			};
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace DotnetBunnyLogBrowser
 {
